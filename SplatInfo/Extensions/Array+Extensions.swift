2bea21ce10de664d59ccb1921148f5fc569d7914
Görkem Güclü <goerkem.g@gmail.com>
2023-12-07 15:21:48 +0100

    Added Salmon Run boss images. Removed thumbnail images of stages and weapons.

---
+++ SplatInfo/Extensions/Array+Extensions.swift
@@ -0,0 +1,8 @@
+//
+//  Array+Extensions.swift
+//  SplatInfo
+//
+//  Created by Görkem Güclü on 07.12.23.
+//
+
+import Foundation
---
+++ SplatInfo/Extensions/UIViewRepresentable+Extensions.swift
@@ -0,0 +1,8 @@
+//
+//  UIViewRepresentable+Extensions.swift
+//  SplatInfo
+//
+//  Created by Görkem Güclü on 26.11.23.
+//
+
+import Foundation
